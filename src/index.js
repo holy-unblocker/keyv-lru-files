f6a2bd8818dcc152567b7e247511caef921df6be
Simon Cheng <loopingthroughlife@gmail.com>
2022-05-02 17:53:19 -0400

    support absolute paths
    use path.resolve instead of path.join

--- src/index.js
+++ src/index.js
@@ -27,7 +27,7 @@ class FileCache {
 
     // determine cache directory
     if (!opts.hasOwnProperty("dir") || typeof opts.dir !== "string") opts.dir = "cache";
-    o.dir = path.join(require.main.filename, "..", opts.dir);
+    o.dir = path.resolve(require.main.filename, "..", opts.dir);
 
     // determine maximal total number of files
     opts.files = (!opts.hasOwnProperty("files")) ? false : parseInt(opts.files, 10);
